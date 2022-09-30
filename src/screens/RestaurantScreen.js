d593a17b3267fddec1764571413ac5608ecadd7f
Aleksandra Haniok <a.haniok@gmail.com>
2022-09-30 12:21:49 +0100

    Add useRestaurant hook to fetch and display photos for a specific restaurant

--- src/screens/RestaurantScreen.js
+++ src/screens/RestaurantScreen.js
@@ -1,12 +1,75 @@
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Dimensions, FlatList, Image, Text, View } from "react-native";
+import yelp from "../api/yelp";
 
-export default function RestaurantScreen({navigation}) {
-    const id = navigation.getParam('id')
+function RestaurantScreen({ navigation }) {
+  const id = navigation.getParam("id");
+  const [results, setResults] = useState({
+    data: null,
+    loading: false,
+    error: null,
+  });
 
+  useEffect(() => {
+    fetchRestaurant();
+  }, []);
+
+  const dimensions = Dimensions.get("window");
+  const imageHeight = Math.round((dimensions.width * 9) / 16);
+  const imageWidth = dimensions.width;
+
+  const fetchRestaurant = async () => {
+    setResults({
+      data: null,
+      loading: true,
+      error: null,
+    });
+    try {
+      const response = await yelp.get(`/${id}`);
+
+      setResults({
+        data: response.data,
+        loading: false,
+        error: null,
+      });
+    } catch (error) {
+      setResults({
+        data: null,
+        loading: false,
+        error: "something went wrong",
+      });
+    }
+  };
+
+  if (results.loading)
+    return <ActivityIndicator size='large' marginVertical={30} />
+
+  if (results.error)
     return (
-        <View>
-            <Text>Hello Restaurant Screen</Text>
-            <Text>{id}</Text>
-        </View>
-    )
+      <View>
+        <Text>Inside error</Text>
+      </View>
+    );
+
+  return (
+    <View>
+      {results.data && (
+        <FlatList
+          data={results.data.photos}
+          keyExtractor={(photo) => photo}
+          renderItem={({ item }) => {
+            console.log({ item });
+            return (
+              <Image
+                style={{ height: imageHeight, width: imageWidth }}
+                source={{ uri: item }}
+              />
+            );
+          }}
+        />
+      )}
+    </View>
+  );
 }
+
+export default RestaurantScreen;
