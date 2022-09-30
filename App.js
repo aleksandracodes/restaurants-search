472a8b95e00007ae15e1215d1bd9b6e44c215e28
Aleksandra Haniok <a.haniok@gmail.com>
2022-09-30 11:07:58 +0100

    Add the RestaurantScreen component

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import RestaurantScreen from "./src/screens/RestaurantScreen";
 
 // install --> npm install react-navigation react-navigation-stack
 const navigator = createStackNavigator({
-  Home: HomeScreen
+  Home: HomeScreen,
+  Restaurant: RestaurantScreen
   }, 
   {
-  initialRouteName: 'Home',
+  initialRouteName: 'Restaurant',
   defaultNavigationOptions: {
     title: 'BuisnessSearch'
   }
---
+++ src/screens/RestaurantScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function RestaurantScreen() {
+    return (
+        <View>
+            <Text>Hello Restaurant Screen</Text>
+        </View>
+    )
+}
