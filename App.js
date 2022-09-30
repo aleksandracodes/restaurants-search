5fb47d237d4ad7517c7b0960deb54e4968be2781
Aleksandra Haniok <a.haniok@gmail.com>
2022-09-30 11:02:32 +0100

    Create a HomeScreen and return from the App.js

--- App.js
+++ App.js
@@ -1,57 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import Categories from './src/components/Categories';
-import Header from './src/components/Header';
-import Restaurants from './src/components/Restaurants';
-import Search from './src/components/Search';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
 
-export default function App() {
-  const [term, setTerm] = useState('Burger');
+// install --> npm install react-navigation react-navigation-stack
+const navigator = createStackNavigator({
+  Home: HomeScreen
+  }, 
+  {
+  initialRouteName: 'Home',
+  defaultNavigationOptions: {
+    title: 'BuisnessSearch'
+  }
+})
 
-  const commonCategories = [
-    {
-      name:'Burger',
-      imageUrl: require('./src/assets/images/burger.png')
-    },
-    {
-      name:'Pizza',
-      imageUrl: require('./src/assets/images/pizza.png')
-    },
-    {
-      name:'Dessert',
-      imageUrl: require('./src/assets/images/cake.png')
-    },
-    {
-      name:'Drinks',
-      imageUrl: require('./src/assets/images/smoothies.png')
-    },
-    {
-      name:'Steak',
-      imageUrl: require('./src/assets/images/steak.png')
-    },
-    {
-      name:'Pasta',
-      imageUrl: require('./src/assets/images/pasta.png')
-    },
-  ]
-
-  return (
-    <View style={styles.container}>
-      <Header />
-      <Search 
-        setTerm={setTerm}
-      />
-      <Categories
-        categories={commonCategories}
-        setTerm={setTerm}
-        term={term}
-      />
-      <Restaurants term={term} />
-      <StatusBar style='auto' />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-});
+export default createAppContainer(navigator)
